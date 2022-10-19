eb986a4050891a07b555c39a67c9516ddabe6c10
Sojworld <sojiolanipekun@gmail.com>
2022-10-19 00:32:20 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,7 @@
  * _isalpha - checks for alphabets
  * @c: character to be checked
  *
- * Return: 1 if character is a letter,0 otherwise
+ * Return: 1 if character is a letter, 0 otherwise
  */
 int _isapha(inc c)
 {
