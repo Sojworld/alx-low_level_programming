0a2084e6406efd50f0cf636616a35f281d4ae802
Sojworld <sojiolanipekun@gmail.com>
2022-10-20 16:07:50 -0700

    task7

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more numbers - print more numbers
+ */
+
+
+
+void more_unmbers(void)
+
+{
+	int i, j;
+	for (i = 1; 1 <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar('1');
+			_putchar (j % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
