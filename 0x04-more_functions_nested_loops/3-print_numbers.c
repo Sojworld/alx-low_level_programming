fddf6c0e78bc2fef941f64a767b70b70808b18f8
Sojworld <sojiolanipekun@gmail.com>
2022-10-20 21:54:13 -0700

    newtask

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-#inclusde "main.h"
+#include "main.h"
+
 
 /**
  * print_numbers - function that print all numbers from 0 to 9
+ *
  * Return: returns void
  */
+
 
 void print_numbers(void)
 {
