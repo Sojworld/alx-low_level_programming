8a3248a6fd170f39540d1f7b8530fdc472848ac3
Sojworld <sojiolanipekun@gmail.com>
2022-10-21 03:55:04 -0700

    task8

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -4,22 +4,24 @@
 /**
  * print_diagonal - draws a diagonal line
  * @n: numbers of '\' to be printed
- * Return: Always 0.
+ * Return: a diagonal
  */
 void print_diagonal(int n)
 {
 	int x, y;
 
 	if (n <= 0)
+	{
 		_putchar('\n');
+	}
+	else
 
 	for (x = 0; x < n; x++)
 	{
 		for (y = 0; y < x; y++)
 			_putchar(' ');
+	}
 
 		_putchar('\\');
 		_putchar('\n');
-	}
-	return (0);
 }
