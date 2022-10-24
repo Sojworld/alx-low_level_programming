d07bd171273c3ba91425dcdf0628b58235056e3f
Sojworld <sojiolanipekun@gmail.com>
2022-10-24 06:54:06 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,14 +2,19 @@
 
 /**
  * swap_int - swap values of two nums
- * @b: first pointer
- * @a: second pointer
+ * @a: first pointer
+ * @b: second pointer
  * Return: void
  */
 
 void swap_int(int *a, int *b)
 {
-	int constant = *a;
-	*b = *a;
-	*a = constant;
+	int c;
+
+	c = *a
+
+	*a = *b;
+
+	*b = c;
+
 }
