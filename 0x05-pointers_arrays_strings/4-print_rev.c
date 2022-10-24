6910b0f054b46529b34dece5b40af24c8ff1695e
Sojworld <sojiolanipekun@gmail.com>
2022-10-24 08:34:39 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string to pring
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int = 0;
+
+	while (*(s + i))
+		i++;
+	i = i - 1;
+	while (i >= o)
+	{
+		_putchar(*(s + i));
+		i--;
+	}
+	_putchar('\n');
+}
