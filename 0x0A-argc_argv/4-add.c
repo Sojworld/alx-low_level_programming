8380e6d5a6bbec6b78c1c78d5c7530b3d966137c
Sojworld <sojiolanipekun@gmail.com>
2022-11-04 06:17:12 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,29 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include <main.h>
+#define UNUSED(x)(void)(x)
 /**
- * main - Entry Point
- * @argc: arguments
- * @argv: array pointing to arguments
- * Return: 0
+ * main - adds two numbers
+ * @argc:int
+ * @argv:array of strings
+ * Return: sum if all digits
  */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i;
+	int j;
+	int c;
+	int sum;
+	char *s;
 
-	if (argc < 1)
-		return (0);
+	sum = 0;
+	c = 0;
+
+	i = 1;
+	while (i < argc)
+	{
+		s = argv[i];
+		j = 0;
+		while (s[j] != '\0')
+		{
+			if (s[j] < '0' || s[j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			j++;
+		}
+		i++;
+	}
 
 	for (i = 1; i < argc; i++)
 	{
-		if (!atoi(argv[i]))
-		{
-			printf("%s\n", "Error");
-			return (1);
-		}
-		sum += atoi(argv[i]);
+		c = atoi(argv[i]);
+		sum = sum + c;
 	}
 	printf("%d\n", sum);
-
 	return (0);
 }
